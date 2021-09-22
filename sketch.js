295d4dbe475c3fd2db1b30eee825acbe9790e990
jishnuthewalker <jishnuthewalker@gmail.com>
2021-09-22 15:03:53 +0530

    scaling circles

--- sketch.js
+++ sketch.js
@@ -5,11 +5,32 @@ function setup() {
 
 function draw() {
   background(200);
-  length = mouseY/5;
-  n = 0;
-  while (n < length) {
-    n++;
-    point(windowWidth/2 + n, mouseY);
-    rect(windowWidth/8, 60, mouseX, 40);
+
+  //while loop test
+
+/*  length = mouseY/5;
+    i = 0;
+    while (i < length) {
+    rectPosY = windowWidth/8;
+    i++;
+    point(windowWidth/2 + i, mouseY);
+    rect(windowWidth/8, 60, mouseX - rectPosY, 40);
+  } */
+
+/*  length = mouseY/5;
+    for (var i = 0; i < length; i++) {
+    rectPosY = windowWidth/8;
+    point(windowWidth/2 + i, mouseY);
+    rect(windowWidth/8, 60, mouseX - rectPosY, 40)
+  }
+  */
+
+//  diam = 100;
+  diam = mouseY/5 + 50;
+  for (y = 0; y < height; y += diam) {
+    for (x = 0; x < width; x += diam) {
+      fill(255, 140, 36);
+      ellipse(x, y, diam, diam);
+    }
   }
 }
