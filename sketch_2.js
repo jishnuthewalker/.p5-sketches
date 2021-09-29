ab404b846f7b234636500abbfbdac1af918f746e
jishnuthewalker <jishnuthewalker@gmail.com>
2021-09-29 19:54:22 +0530

    added camera

--- sketch_2.js
+++ sketch_2.js
@@ -8,6 +8,9 @@ function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   graphics = createGraphics(200,200);
   graphics.background(255);
+//  camera = createCamera();
+  createEasyCam();
+  document.oncontextmenu = function() { return false; }
 
 
 //  slider.style('width', '80px');
@@ -16,12 +19,21 @@ function setup() {
 
 function draw() {
   background(255);
+  rotX = 0;
+  rotY = 0;
+  rotZ = 0;
   valX = slidX.value();
   valY = slidY.value();
   valZ = slidZ.value();
+  /* if (mouseIsPressed) {
+      rotX = pmouseX + rotX;
+      rotY = pmouseY = rotY;
+  //    rotateX((rotX * 3.14) / 180);
+  //    rotateY((rotY * 3.14) / 180);
+}*/
   square(200, 200, 200);
-  rotateX(0.5);
-  rotateY(0.5);
+  rotateX(0.5 + (rotX * 3.14) / 180);
+  rotateY(0.5 + (rotY * 3.14) / 180);
   rotateZ(1.3);
   box(valX, valY, valZ);
 
