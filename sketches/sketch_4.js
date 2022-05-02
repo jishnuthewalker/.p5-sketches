dbb3a9e2ca6e4e2fe2c895a87921b8bb70a98199
jishnu <jishnuthewalker@gmail.com>
2022-05-03 03:37:34 +0530

    hbkjk

--- sketches/sketch_4.js
+++ sketches/sketch_4.js
@@ -6,7 +6,11 @@ let PALETTE = [];
 
 
 function setup() {
+
   createCanvas(windowWidth, windowHeight, SVG);
+  noLoop();
+  angleMode(DEGREES);
+  rectMode(CENTER);
 
   PALETTE = [
     color(255, 200, 200),
@@ -14,33 +18,32 @@ function setup() {
     'limegreen'
   ]
 
-  noLoop();
-  angleMode(DEGREES);
-  rectMode(CENTER);
-
-
 }
 
 function draw() {
-  testLines();
+//  testLines();
+  outlineShape();
 }
+
+function outlineShape() {
+
+  const strokeColor = getRandomFromPalette();
+  const weight = randomSelectTwo() ? 1 : 3;
+
+  stroke(strokeColor);
+  strokeWeight(weight);
+  push();
+  translate(width/2,height/2);
+  rotate(0);
+  ellipse(0,0,CRYSTAL_SIZE,CRYSTAL_SIZE);
+  pop();
+}
+
+
 
 function testLines() {
 
-  const rando = random(1);
-  let numShapes
-  if (rando > 0.5) {
-    numShapes = SIDES;
-  }
-  else {
-    numShapes = SIDES * 2;
-  }
-console.log(rando)
-
-const rando2 = floor(random(0, PALETTE.length));
-console.log(rando2);
-const colPick = PALETTE[rando2];
-
+  let numShapes = randomSelectTwo() ? SIDES : SIDES*2 ;
 
   background(255);
   fill('teal');
@@ -49,11 +52,27 @@ const colPick = PALETTE[rando2];
    stroke(PALETTE[1]);
    translate(width/2, height/2);
    ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE);
-  stroke(colPick);
-  const angle = 360 / numShapes;
+
+   stroke(getRandomFromPalette());
+   const angle = 360 / numShapes;
    for (i = 0; i < numShapes; i++) {
      line(0,0,CRYSTAL_SIZE/2,0);
      rotate(angle);
    }
   pop();
+}
+
+
+function randomSelectTwo() {
+  const rando = random(1) > 0.5 ? true : false;
+  console.log(rando);
+  return(rando);
+
+}
+
+
+function getRandomFromPalette() {
+   const rando2 = floor(random(0, PALETTE.length));
+   console.log(rando2);
+   return PALETTE[rando2];
 }
